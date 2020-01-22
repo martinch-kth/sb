da4989d72cd55e3592582a0fac44219e900dcbfd
martinch-kth <martinch@kth.se>
2020-01-22 16:38:57 +0100

    ..

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import { Statistic,Grid, Placeholder, Menu, Segment,Form, Checkbox } from 'semantic-ui-react'
+
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+
+      return (
+
+          <Grid>
+
+
+            <Grid.Row>
+              <Grid.Column width={13}>
+                <Segment raised>SAAB - Treemap </Segment>
+              </Grid.Column>
+            </Grid.Row>
+
+          </Grid>
+      )
+    }
+
+} export default App;
+
+
+
+
